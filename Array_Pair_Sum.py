2b57ead8ffe1946cef78d7e05297a72bae391b0c
Alex <98416718+alexandru-ghibea@users.noreply.github.com>
2022-12-16 17:40:24 +0200

    Create Array_Pair_Sum.py

---
+++ Array_Pair_Sum.py
@@ -0,0 +1,29 @@
+#Array Pair Sum
+
+# Problem
+
+# Given an integer array, output all the ** unique ** pairs that sum up to a specific value k.
+
+# So the input:
+
+# pair_sum([1,3,2,2],4)
+
+# would return 2 pairs:
+
+#  (1,3)
+#  (2,2)
+
+def pair_sum(arr,k):
+    if len(arr) < 2:
+        return
+    seen = set()
+    output = set()
+    
+    for number in arr:
+        target = k - number
+        
+        if target not in seen:
+            seen.add(number)
+        else:
+            output.add( ((min(number, target)), max(number,target)))
+    return len(output)
